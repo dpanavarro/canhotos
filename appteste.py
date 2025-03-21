0f33c9c064210c52c1e14d6aacbe0fdc91fdc07c
Panavarro <panavarro@panavarro.com.br>
2025-03-20 21:23:47 -0300

    Add files via upload

--- appteste.py
+++ appteste.py
@@ -11,6 +11,7 @@ def conectar_banco():
     return pymysql.connect(host="186.224.105.220", port=3306, user="panavarr", password="331sbA8g?", database="panavarr_", charset='utf8mb4')
 
 # Salvar imagem no banco
+# Salvar imagem no banco
 def salvar_imagem_no_banco(imagem, nota_fiscal, info_envio):
     conn = conectar_banco()
     cursor = conn.cursor()
@@ -32,28 +33,28 @@ def salvar_imagem_no_banco(imagem, nota_fiscal, info_envio):
     cursor.close()
     conn.close()
 
-# Ler QR Code usando OpenCV
-def ler_qrcode_opencv(imagem_pil):
+# Ler Código de Barras usando OpenCV
+def ler_codigo_barras(imagem_pil):
     imagem_cv = np.array(imagem_pil)
-    detector = cv2.QRCodeDetector()
-    data, bbox, _ = detector.detectAndDecode(imagem_cv)
-    if bbox is not None and data:
-        return data
+    detector = cv2.barcode.BarcodeDetector()
+    retval, decoded_info, _, _ = detector.detectAndDecode(imagem_cv)
+    if retval:
+        return decoded_info[0]
     return None
 
 # Streamlit UI
-st.title('📸 Captura Automática com Leitura de QR Code')
+st.title('📸 Captura Automática com Leitura de Código de Barras')
 
-img_camera = st.camera_input("Capture a imagem com QR Code")
+img_camera = st.camera_input("Capture a imagem com Código de Barras")
 
 if img_camera:
     imagem_pil = Image.open(img_camera)
     st.image(imagem_pil, caption='Imagem capturada', use_container_width=True)
 
-    nota_fiscal = ler_qrcode_opencv(imagem_pil)
+    nota_fiscal = ler_codigo_barras(imagem_pil)
 
     if nota_fiscal:
-        st.success(f"QR Code detectado: {nota_fiscal}")
+        st.success(f"Código de barras detectado: {nota_fiscal}")
 
         tipo_envio = st.radio("Selecione o tipo de envio:", ["Motorista", "Transportadora"])
         nome_motorista = st.text_input("Nome do Motorista (se aplicável)", "") if tipo_envio == "Motorista" else "Transportadora"
@@ -63,4 +64,4 @@ if img_camera:
             st.success("Imagem e Nota Fiscal salvas com sucesso!")
 
     else:
-        st.error("Nenhum QR Code detectado. Tente novamente com uma imagem mais clara.")
+        st.error("Nenhum código de barras detectado. Tente novamente com uma imagem mais clara.")
